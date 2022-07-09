9951bcc91033518acc6f6a2b119d6de8c46ca6b7
aryan Jadhav <aryanj0808@gmail.com>
2022-07-09 23:59:46 +0530

    Creating and connected with admin database

--- Project01/Home/views.py
+++ Project01/Home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from Home.models import Contact
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -7,6 +8,13 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name, email=email ,phone=phone, desc=desc, date=datetime.today())
+        contact.save()
     return render(request, 'contact.html')
 def services(request):
     return render(request, 'services.html')
